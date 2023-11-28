ab70fed1c338d948fc554d93706611e73210e60f
Laura TAMIC <tamiclaura@gmail.com>
2023-11-28 15:54:53 +0100

    debut_back

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,6 +1,11 @@
 class ProfileController < ApplicationController
   def show
-    @user = Current_user.find(params[:id])
-    # @user = User.new
+    @user = current_user
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:name, :user_id, photos: [])
   end
 end
