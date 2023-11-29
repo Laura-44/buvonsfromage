0387decfb2d0ffbe86a6211010e99d0549b7b4dc
marion thorin <marion.thorin@gmail.com>
2023-11-29 17:49:41 +0100

    searching stimulus index categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,49 +1,24 @@
 class CategoriesController < ApplicationController
 
-
-
-  # je veux :
-
-  def index
-    @category = Category.find(4)
-    if @category.name == "Cheese"
-      @choices = Category.find_by(name: "Beer").drinks
-      # @choices = Category.find_by(name: "Wine").drinks
-    else
-      @choices = Category.find_by(name: "Cheese").foods
-    end
-
-
-
-
-
-
+    def index
+      @category = Category.find_by(params[:category_id])
+      @second_category = Category.find_by(params[:second_category_id])
+      if @category.name == "Cheese"
+        @choices = Category.find_by(name: "Beer").drinks
+        # @choices = Category.find_by(name: "Wine").drinks
+      else
+        @choices = Category.find_by(name: "Cheese").foods
+      end
     # @choices.shuffle! if @choices.present?
     # if @category.nil?
     # end
-
   end
 
-  # category_drinks = CategorieDrink.where(category = Category.where(name: "beer"))
-    # récupérer une image correspondant au choix de catégorie de l'utiilisateur (qui se trouve dans food_controller : "quel produit avez-vous ?")
-    # récupérer une image de la seed correspondant au choix de catégorie de l'utilisateur (dans food_controller : "avec quel produit voulez-vous l'associer ?")
-
-    # Afficher les Categorys de la seed
-
-
-
-    # avec show il faut recharger la page à chaque scroll sur la droite, et changer d'url à chaque fois
-    # avec index on itère sur tous les Categoryes dans la seed et ensuite on recharge la page et on change d'url
-
-# un utilisateur peut voir une sélection de produits qui Categoryent
-# photos, description
 
   private
 
   def categories_params
     params.require(:categories).permit(:name, :category_id, photos: [])
   end
-
-# SELECT category WHERE  categorie.name INCLUDE beer AND wine
 
 end
--- app/controllers/pairings_controller.rb
+++ app/controllers/pairings_controller.rb
@@ -1,11 +1,24 @@
 class PairingsController < ApplicationController
 
+  def new
+    @pairing = Pairing.new
+  end
+
   def create
-    @pairing = Pairing.new(pairing_params)
+    @pairing = Pairing.new(params[:id])
     @pairing.user = current_user
     @pairing.save
-    # redirect_to pairing_path(@pairing) : doit renvoyer à la page des pairings cad du "panier"
-    # attention : pas de view encore créée pour le pairings controller
+    redirect_to categories_path
+  end
+
+
+  private
+
+  def pairings_params
+    params.require(:pairings).permit(:pairing_id, :food_id, :drink_id, :user_id, photos: [])
   end
 
 end
+
+# redirect_to pairing_path(@pairing) : doit renvoyer à la page des pairings cad du "panier"
+# attention : pas de view encore créée pour le pairings controller
